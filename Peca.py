45f21b003d182fff4a42fbfedb9b9a302c20be77
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-01-17 14:49:37 -0200

    Criada classe em arquivo separado

---
+++ Peca.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# coding=UTF-8
+
+class Peca:
+    """Peças presentes no tabuleiro incluindo as relativas ao agente (
+    Mão, sino, olho, etc.)"""
+    def __init__(self, imagem=None, linha=0, coluna=0, acoes_inerentes=None):
+        self.imagem = imagem
+	self.linha = linha
+	self.coluna = coluna
+	# TODO: As ações inerentes variam de acordo com regras do domínio
+	self.acoes_inerentes = acoes_inerentes
