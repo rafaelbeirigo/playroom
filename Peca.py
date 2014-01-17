0f2bf9823640dfbaafde37b550f6b97fcd5d23ea
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-01-17 15:13:29 -0200

    Adicionada propriedade "nome" à classe "Peca"

--- Peca.py
+++ Peca.py
@@ -4,7 +4,8 @@
 class Peca:
     """Peças presentes no tabuleiro incluindo as relativas ao agente (
     Mão, sino, olho, etc.)"""
-    def __init__(self, imagem=None, linha=0, coluna=0, acoes_inerentes=None):
+    def __init__(self, nome=None, imagem=None, linha=0, coluna=0, acoes_inerentes=None):
+        self.nome = nome
         self.imagem = imagem
 	self.linha = linha
 	self.coluna = coluna
