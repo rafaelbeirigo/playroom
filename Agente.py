aea7249cf5d6666a89e06cbb469356998bcd1f38
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-01-17 17:02:15 -0200

    Criada lista com ações de movimentação do olho

--- Agente.py
+++ Agente.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python
 # coding=UTF-8
+import random
 
 class Agente:
     """Classe referente ao agente aprendiz"""
+    def __init__(self, pecas):
+        self.acoes_disponiveis = [
+            "move_olho_uma_celula_norte",
+            "move_olho_uma_celula_sul",
+            "move_olho_uma_celula_leste",
+            "move_olho_uma_celula_oeste"
+        ]
+        self.pecas = pecas
+
+if __name__ == "__main__":
+    agente = Agente()
+    
+    acao_aleatoria = random.randint(0, len(agente.acoes_disponiveis) - 1)
+    print "Ação aleatória escolhida: " + agente.acoes_disponiveis[acao_aleatoria]
