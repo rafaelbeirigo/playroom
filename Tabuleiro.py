82354e865575a14848f1631851b6a9c415a4834c
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-01-17 14:49:04 -0200

    Criada classe em arquivo separado

---
+++ Tabuleiro.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# coding=UTF-8
+
+class Tabuleiro:
+    """Tabuleiro onde as peças são posicionadas"""
+    def __init__(self, linhas, colunas):
+	self.linhas = linhas
+	self.colunas = colunas
