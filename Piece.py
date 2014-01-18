632802d88e49105c2cd00792eb053ec1b2235f1a
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-01-18 13:42:21 -0200

    Removido acoes_inerentes da classe Piece

--- Piece.py
+++ Piece.py
@@ -3,10 +3,8 @@
 
 class Piece:
     """Pieces present on the board (including those pertaining to the agent)"""
-    def __init__(self, name=None, image=None, row=0, column=0, inherent_actions=None):
+    def __init__(self, name=None, image=None, row=0, column=0):
         self.name = name
         self.image = image
 	self.row = row
 	self.column = column
-	# TODO: As ações inerentes variam de acordo com regras do domínio
-	self.inherent_actions = inherent_actions
