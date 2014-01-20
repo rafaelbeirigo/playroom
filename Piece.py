1af2befd446813c11e3317f256cc9325aa736e61
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-01-20 21:08:34 -0200

    Adicionar à instanciação de cada peça as ações inerentes a ela

--- Piece.py
+++ Piece.py
@@ -3,11 +3,12 @@
 
 class Piece:
     """Pieces present on the board (including those pertaining to the agent)"""
-    def __init__(self, name=None, image=None, row=0, column=0):
+    def __init__(self, name=None, image=None, row=0, column=0, actions=[]):
         self.name = name
         self.image = image
 	self.row = row
 	self.column = column
+        self.actions = actions
 
     def get_actions():
-        return []
+        return self.actions
