3ddce65541d012b06ec0e1c5516bf4c9b8baefad
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-01-28 18:39:23 -0200

    Copiado trecho do Q-Learning para adaptar neste projeto

--- Playroom.py
+++ Playroom.py
@@ -611,6 +611,73 @@ def get_Q_value(key):
         Q[key] = Q_default_value
     return Q[key]
 
+def q_learning_simple():
+    myAgent = self.Agent
+    myMDP = self.MDP
+    A = self.MDP.A
+
+    self.initializeQ()
+    Q = self.Q
+
+    alpha            = self.alpha
+    gamma            = self.gamma
+    gammaPRQL        = self.gammaPRQL
+    epsilon          = self.epsilon
+    epsilonIncrement = self.epsilonIncrement
+
+    W2 = 0
+    Ws = []
+    for k in range(self.K):
+        W = 0
+        myAgent.setInitialState()
+        #myAgent.state = '1'
+        for h in range(self.H):
+            # TODO: remover os '---'
+            # ---Observe the current state s
+            s = myAgent.state
+
+            # if a goal state is reached the episode ends
+            if s in myMDP.G: break
+
+            # ---Following epsilon-greedy strategy,
+            # ---Select an action a and execute it
+            # ---Receive immediate reward r
+            # ---Observe the new state s2
+            randomNumber = random()
+            if randomNumber <= epsilon:
+                # random
+                a = myAgent.selectRandomAction()
+            else:
+                # greedy
+                a = myAgent.selectBestAction(s, source = 'Q-Table', Q = Q)
+
+            s2, r = myAgent.executeAction(a)
+
+            # TODO: manter um vetor V com os maximos
+            maxValue = -1.0
+            for a2 in A:
+                if Q[s2][a2] > maxValue:
+                    maxValue = Q[s2][a2]
+
+            # ---Update the table entry for Q(s, a)
+            Q[s][a] = (1.0 - float(alpha)) * float(Q[s][a]) + \
+                      float(alpha) * (float(r) + float(gamma) * float(maxValue))
+
+            # ---s=s'
+            myAgent.state = s2
+
+            if gammaPRQL != None:
+                # accumulate gamma on W (this value is used only in PRQLearning)
+                W = float(W) + pow(gammaPRQL, h) * r
+
+        epsilon = epsilon + epsilonIncrement
+
+        W2 = ( (W2 * k) + W ) / (k + 1)
+        Ws.append(W2)
+
+    self.Q = Q
+    return W2, Ws
+
 root = tk.Tk()
 
 # bottomframe = Frame(root)
