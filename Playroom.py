4716f00e14427c100cae80521594b15c0948ab5e
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-03-17 14:47:34 -0300

    Remover adição na list non_agent_pieces

--- Playroom.py
+++ Playroom.py
@@ -906,7 +906,7 @@ eye = Piece(name = "eye", image = tk.PhotoImage(file="/home/rafaelbeirigo/cienci
 marker = Piece(name = "marker", image = tk.PhotoImage(file="/home/rafaelbeirigo/ciencia/playroom/img/target.gif"), row=1, column=2)
 
 agent_pieces = [hand, eye, marker]
-non_agent_pieces = [ball, bell, blue_block, red_block, switch, toy_monkey]
+non_agent_pieces = [switch]
 all_pieces = agent_pieces + non_agent_pieces
 
 for piece in non_agent_pieces:
