5a13191deb44992174a2f7e3f9e1a80c382511aa
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-04-22 03:12:07 -0300

    r_i-2014-04-22_03-12-07.log

--- Playroom.py
+++ Playroom.py
@@ -1609,7 +1609,7 @@ def imrl():
             current_option = next_action
             a2 = get_action_from_option(s2, next_action)
         else:
-            print 's2: ' + s2
+            print 's2: ' + str(s2)
             beta = get_BETA(current_option, s2)
             print 'BETA: ' + str(beta)
             current_option = None
