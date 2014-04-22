60d80e8b5da4f706afd3a08b993a52d43d7e3c9e
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-04-22 00:32:31 -0300

    alpha == .1

--- Playroom.py
+++ Playroom.py
@@ -1427,7 +1427,7 @@ def imrl():
     board.update_screen = False
 
     # Learning parameters
-    alpha            = 0.5
+    alpha            = 0.1
     gamma            = 0.9
     epsilon          = 0.1
     tau              = 0.9
