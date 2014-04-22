71ebc52abfa719fc06adc7d1b04f31c0c0a3324f
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-04-21 22:15:41 -0300

    epsilon = .5

--- Playroom.py
+++ Playroom.py
@@ -1418,7 +1418,7 @@ def imrl():
     # Learning parameters
     alpha            = 0.1
     gamma            = 0.9
-    epsilon          = 0.2
+    epsilon          = 0.5
     tau              = 0.9
 
     steps = int(1e5)
