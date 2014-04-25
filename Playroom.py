8b9d70dfc4fc84aefcd0f194f031488f25c2d393
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-04-25 05:00:28 -0300

    Mais passos

--- Playroom.py
+++ Playroom.py
@@ -1449,7 +1449,7 @@ def imrl():
     epsilon          = 0.25
     tau              = 0.9
 
-    steps = int(2e4)
+    steps = int(2.5e5)
 
     # Log stuff
     r_i_filename = get_log_filename(prefix='r_i-')
