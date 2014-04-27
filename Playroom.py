ebca618e7d1b2bdc5b7b25ecf8aa77033aad68a3
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-04-27 19:56:17 -0300

    Unificação dos testes de abandono de option e seleção de uma nova ação

--- Playroom.py
+++ Playroom.py
@@ -1595,7 +1595,7 @@ def imrl():
         # If the option took the agent to a state that isn't in I yet,
         if current_option == None \
            or get_BETA(current_option, s2) == 1.0 \
-           or s2 not in get_I(current_option)):
+           or s2 not in get_I(current_option):
             current_option = None # The option will stop being followed
             if random() < epsilon:    # random() gives a number in the interval [0, 1).
                 # random
