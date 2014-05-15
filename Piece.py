1a2c358988b2c59bb02a3e5a0aef1d4a4a9fa95d
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-05-15 20:09:02 -0300

    Pieces now have value

--- Piece.py
+++ Piece.py
@@ -3,12 +3,13 @@
 
 class Piece:
     """Pieces present on the board (including those pertaining to the agent)"""
-    def __init__(self, name=None, image=None, row=0, column=0, actions=[]):
+    def __init__(self, name=None, image=None, row=0, column=0, actions=[], value=0):
         self.name = name
         self.image = image
 	self.row = row
 	self.column = column
         self.actions = actions
+        self.value = value
 
     def get_actions(self):
         return self.actions
