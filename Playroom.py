793fd0393eff0455e782c08bdef780d3842ef9d9
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-05-17 17:18:17 -0300

    Teste: 2e4 passos

--- Playroom.py
+++ Playroom.py
@@ -1559,7 +1559,7 @@ def imrl():
         epsilon          = 0.1
         tau              = 0.5
 
-        steps = int(1e4)
+        steps = int(2e4)
 
         # Variables initialization
         s = update_state()
