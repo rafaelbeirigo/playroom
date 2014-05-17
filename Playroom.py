717dfb628ec75ff71e77dd87dd81890d9c1d6248
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-05-17 16:05:52 -0300

    NOT persisting O and experimental data!

--- Playroom.py
+++ Playroom.py
@@ -1761,15 +1761,15 @@ def imrl():
         if args.log_step: log_step(s2, current_option, a2, r_i2)
         if args.log_option_stack: log_option_stack()
 
-        # Persist data related to this experiment
-        if random.random() < 1e-4:
-            if ED_filename == None:
-                if r_i_filename == None:
-                    r_i_filename = get_log_filename(prefix='r_i-')
-                ED_filename = r_i_filename + '.dat'
-            saveobject([args, alpha, gamma, epsilon, tau, r_i_filename, step_filename, option_stack_filename, ED_filename, s, s2, S, a, a2, r_e, r_e2, r_i, r_i2, current_option, option_stack, current_step, steps, Q, O, Vx, Ax], ED_filename)
+        # # Persist data related to this experiment
+        # if random.random() < 1e-4:
+        #     if ED_filename == None:
+        #         if r_i_filename == None:
+        #             r_i_filename = get_log_filename(prefix='r_i-')
+        #         ED_filename = r_i_filename + '.dat'
+        #     saveobject([args, alpha, gamma, epsilon, tau, r_i_filename, step_filename, option_stack_filename, ED_filename, s, s2, S, a, a2, r_e, r_e2, r_i, r_i2, current_option, option_stack, current_step, steps, Q, O, Vx, Ax], ED_filename)
 
-    saveobject(O, get_log_filename(prefix='O-')) # persists O
+    # saveobject(O, get_log_filename(prefix='O-')) # persists O
 
     sys.exit()
 
