30f9656781e0f32f632ec44d5dbdd19253722fb1
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-07-02 15:59:43 -0300

    Adaptada a obtenção de r_i2

--- Playroom.py
+++ Playroom.py
@@ -1693,7 +1693,7 @@ def imrl():
                                                 dtype=scipy.float32),
                                     dtype=scipy.float32)
 
-            r_i2 = tau * (1.0 - get_P(o, s2, s))
+            r_i2 = tau * (1.0 - P[s][0, s2])
             log_r_i(r_i2, s, s2, current_option, a)
         else:
             r_i2 = 0.0
