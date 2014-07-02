7776ae1d6ce94d5f6df978f2aabbab0da11ad42e
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-07-02 20:31:05 -0300

    r_i-2014-07-02_20-31-05-chloe.log

--- Playroom.py
+++ Playroom.py
@@ -1655,7 +1655,7 @@ def imrl():
         epsilon          = 0.1
         tau              = 0.5
 
-        steps = int(1.5e6)
+        steps = int(1e3)
 
         # Variables initialization
         s = update_state()
