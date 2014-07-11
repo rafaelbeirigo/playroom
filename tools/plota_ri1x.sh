3d7aaea99d931ec22db09612415a7c102380518c
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-07-11 20:42:48 -0300

    Initial commit

---
+++ tools/plota_ri1x.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+LOG_FILE=$1
+SUFIX=$2
+EPS_FILE=$LOG_FILE.eps
+JPG_FILE=$EPS_FILE.jpg;
+PNG_FILE=$EPS_FILE.png
+
+get_log () {
+    cp $LOG_FILE ./a.log
+}
+
+get_eps () {
+    echo 'Getting log'
+    get_log
+    echo 'Gnuploting'
+    gnuplot graph_ri.gnuplot
+    echo 'Moving...'
+    mv graph.eps $EPS_FILE
+}
+
+get_jpg () {
+    gs -sDEVICE=jpeg -dJPEGQ=100 -dNOPAUSE -dBATCH -dSAFER -r300 -sOutputFile=$JPG_FILE $EPS_FILE
+}
+
+get_png () {
+    convert -density 300 $EPS_FILE -flatten $PNG_FILE
+}
+
+publish () {
+    scp $PNG_FILE 143.107.165.124:public_html/b-$SUFIX.png
+}
+
+plot () {
+    i=0
+    while [ true ]
+    do
+	i=`expr $i + 1`
+	echo $i
+	echo 'Getting eps file...'
+	get_eps
+	echo 'Getting image file...'
+	get_png
+	echo 'Publishing image file...'
+	publish
+	echo 'Done.'
+	sleep 60
+    done
+}
+
+plot
+gv --watch $EPS_FILE
+pkill plota.sh
+rm a.log
