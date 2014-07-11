8c38435740eee5d10ca016fab4aad3a56742b63d
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-07-11 17:46:40 -0300

    Adicionados epsopt*.sh

---
+++ epsopt2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+roda () {
+    for epsopt in   0.6 0.9
+    do
+	FOLDER="logs/epsopt/$epsopt"
+	if ! [ -e $FOLDER ]
+	then
+	    mkdir -p $FOLDER
+	fi
+
+	for i in {1..100}
+	do
+	    python Playroom.py --nox --no_cardinal --epsopt=$epsopt
+	done
+    done
+}
+
+roda
