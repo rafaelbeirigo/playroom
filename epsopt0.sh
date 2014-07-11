1cae1843242a03ea6f9429654dff3c144ec649c2
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-07-11 17:42:18 -0300

    epsopt/0.0/r_i-2014-07-11_17-42-18-chloe.log

--- epsopt0.sh
+++ epsopt0.sh
@@ -3,16 +3,17 @@
 roda () {
     for epsopt in 0.0 0.0003
     do
-	for i in {1..100}
-	do
-	    python Playroom.py --nox --no_cardinal --epsopt=$epsopt
-	done
-
 	FOLDER="logs/epsopt/$epsopt"
 	if ! [ -e $FOLDER ]
 	then
 	    mkdir -p $FOLDER
 	fi
+
+	for i in {1..100}
+	do
+	    python Playroom.py --nox --no_cardinal --epsopt=$epsopt
+	done
+
 
 	LOGS="logs/*.log"
 	if [ -e  $LOGS ]
