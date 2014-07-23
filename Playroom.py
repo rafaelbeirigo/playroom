577b4d712ac28cb4f9f1392d9478f26e617783a2
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-07-23 15:50:42 -0300

    r_e == 10.0

--- Playroom.py
+++ Playroom.py
@@ -1099,7 +1099,7 @@ def get_reward():
 def get_r_e():
     """Returns the extrinsic reward."""
     if is_on(toy_monkey_sound) and toy_monkey_sound['step'] == step:
-        return 1e3
+        return 10.0
     else:
         return 0.0
 
