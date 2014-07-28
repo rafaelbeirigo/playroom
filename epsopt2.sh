dc0c432bcd2ac44b7b2502a9dc7d7eac44eee0ad
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-07-28 15:59:17 -0300

    epsopt/0.1/r_i-2014-07-28_15-59-17-Teri.log

--- epsopt2.sh
+++ epsopt2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 roda () {
-    for epsopt in 0.6
+    for epsopt in 0.4
     do
 	FOLDER="logs/epsopt/$epsopt"
 	if ! [ -e $FOLDER ]
@@ -9,7 +9,7 @@ roda () {
 	    mkdir -p $FOLDER
 	fi
 
-	for i in {1..50}
+	for i in {1..100}
 	do
 	    python Playroom.py --nox --no_cardinal --epsopt=$epsopt
 	done
