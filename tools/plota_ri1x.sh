8d76dbc2facc3631eb8b43a2cb8711d1a093197f
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2014-08-03 22:30:56 -0300

    Preparado plot de r_is de pasta completa

---
+++ tools/plota_ri1x.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+LOG_FILE=$1
+SUFIX=$2
+EPS_FILE=$LOG_FILE.eps
+JPG_FILE=$EPS_FILE.jpg;
+PNG_FILE=$EPS_FILE.png
+
+get_log () {
+    cp $LOG_FILE ./a.log
+}
+
+get_eps () {
+    echo 'Getting log'
+    get_log
+    echo 'Gnuploting'
+    gnuplot graph_ri.gnuplot
+    echo 'Moving...'
+    mv graph.eps $EPS_FILE
+}
+
+get_jpg () {
+    gs -sDEVICE=jpeg -dJPEGQ=100 -dNOPAUSE -dBATCH -dSAFER -r300 -sOutputFile=$JPG_FILE $EPS_FILE
+}
+
+get_png () {
+    convert -density 300 $EPS_FILE -flatten $PNG_FILE
+}
+
+publish () {
+    scp $PNG_FILE 143.107.165.124:public_html/b-$SUFIX.png
+}
+
+plot () {
+    echo 'Getting eps file...'
+    get_eps
+}
+
+plot
+rm a.log
